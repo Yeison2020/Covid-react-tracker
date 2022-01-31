fcfb33d352050138d534ed08a6973fcb02b72f9d
Yeison <enmanuelquiterios@gmail.com>
2022-01-31 12:08:23 -0500

    Used fetch instead of axios

--- src/App.js
+++ src/App.js
@@ -2,21 +2,37 @@ import styles from "./App.module.css";
 import Cards from "./components/Cards/Cards";
 import Chart from "./components/Chart/Chart";
 import CountryPick from "./components/CountryPick/CountryPick";
-import { fecthData } from "./api";
-import React from "react";
-class App extends React.Component {
-  async componentDidMount() {
-    const data = await fecthData();
-  }
-  render() {
-    return (
-      <div className={styles.container}>
-        <Cards />
-        <CountryPick />
-        <Chart />
-      </div>
-    );
-  }
-}
+
+import React, { useState, useEffect } from "react";
+const App = () => {
+  const url = "https://covid19.mathdro.id/api";
+  const [data, setData] = useState({
+    confirmed: "",
+    recovered: "",
+    deaths: "",
+    lastUpdate: "",
+  });
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => resp.json)
+      .then((data) => {
+        const { data: confirmed, recovered, deaths, lastUpdate } = data;
+        setData({
+          confirmed: confirmed,
+          recovered: recovered,
+          deaths: deaths,
+          lastUpdate: lastUpdate,
+        });
+      });
+  }, []);
+  console.log(data);
+  return (
+    <div className={styles.container}>
+      <Cards />
+      <CountryPick />
+      <Chart />
+    </div>
+  );
+};
 
 export default App;
